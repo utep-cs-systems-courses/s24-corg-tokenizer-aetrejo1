66f0e2b0d71519c21ebfd1702786f91a6ddf5720
aetrejo1 <aetrejo1@miners.utep.edu>
2024-02-26 21:45:24 -0700

    trying to create make file, and trying to fill out tokenizer

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,17 +5,29 @@
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c) {
+  if (( c = ' ' || '\t') && ( c != 0)) {
+     return 1;
+  }
+  return 0;
+}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c) {
+  if (c != ' ' || c != '        ') {
+    return 1;
+  }
+  return 0;
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated token in zero-terminated str.  Return a zero pointer if 
    str does not contain any tokens. */
-char *token_start(char *str); 
+char *token_start(char *str) {
+  
+  
 
 /* Returns a pointer terminator char following *token */
 char *token_terminator(char *token);
