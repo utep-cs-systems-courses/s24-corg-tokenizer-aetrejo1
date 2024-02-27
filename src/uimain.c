8b162a695a1614d3c68f24014f15e0e2e0697aa8
aetrejo1 <aetrejo1@miners.utep.edu>
2024-02-26 22:08:52 -0700

    was able to add simple ui that prints out what user input

---
+++ src/uimain.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#inlcude "history.h"
+#include "tokenizer.h"
+
+
+
+int main() {
+
+
+  while (1) {
+
+    char test[50];
+
+    printf("please enter something\n");
+
+    fgets(test,sizeof(test), stdin);
+
+    printf(test);
+
+    return 0;
+
+  }
+
+
+  return 0;
+ 
+}
